3b5069edfa2854b2eee8af86a736eb36bf8ef01b
Eric Nguyen <53154237+EricNguyen-B@users.noreply.github.com>
2021-12-26 16:18:25 -0500

    created initial routing paths

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const BoosterPage = (props) => {
+  return(
+  <div>
+    <Link to='/'>home</Link>
+    <h1>BOOSTER PAGE</h1>
+  </div>
+  );
+}
+
+
+
+
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route  path="/boosters" element={<BoosterPage/>}/>
+      </Routes>
     </div>
   );
 }
